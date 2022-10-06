3554b4b7e4382db5eee4562a7c7014151b5bc3d8
LethDuarte <lethduarte@gmail.com>
2022-10-06 15:41:52 -0300

    adicionando modo claro e escuro

--- scripts/script.js
+++ scripts/script.js
@@ -13,12 +13,20 @@ function parenteses(){
 }
 
 function igual(){
-    console.log(Object.values(visor));
-    console.log(visor);
-    
+    var resultado = parseInt(visor.value);
+    console.log(resultado);
 }
 
 function modoEscuro(){
-    // document.getElementsByTagName('main').style.backgroundColor = "black";
-    document.body.style.backgroundColor = "red";
+    document.body.style.backgroundColor = "#332E33";
+    document.getElementById('titulo').style.color = "white";
+    document.getElementById('visor').style.backgroundColor = "#454A4D";
+    document.getElementById('visor').style.color = "white";
+}
+
+function modoClaro(){
+    document.body.style.backgroundColor = "#f5dde0";
+    document.getElementById('titulo').style.color = "black";
+    document.getElementById('visor').style.backgroundColor = "white";
+    document.getElementById('visor').style.color = "black";
 }
